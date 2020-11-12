35ff812e54d5bc60ab44d8b6b124c154b6620cfa
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2020-11-12 18:22:54 +0800

    Update PracticalIBMOCK.java
    Code including teacher suggestions. Please test it, I wrote this online on GitHub, so I could not test it using the compiler.
    Happy Diwali.

--- PracticalIBMOCK.java
+++ PracticalIBMOCK.java
@@ -4,27 +4,35 @@ public class PracticalIBMOCK {
 	public static void main(String[] args) {
 	
 		IBIO.output("Soumil Mittra");
-		int sum = 0;
-		int min = 1000;
+		double sum = 0; // to ensure accurate averages with the right decimal value
+		int min = 0;
 		int max = 0;
 		
 		int num = IBIO.inputInt("Range daal: ");
-		if (num < 5)
+		while (num < 5) // keep asking for number if number is below what we need
 		{
 			IBIO.out("Out of range lahhh cb knn bay chao!!!!!");
-			System.exit(0);
+			num = IBIO.inputInt("Range daal: "); // input again
 		}
 		int n = 0;
 		 for (int i = 0; i < num; i++)
 		 {
 			 n = IBIO.inputInt("enter a number");
-			 if (n < min)
+			 // if this is the first number that we input, set min and max to that first input number, to get accurate min and max values
+			 if(i == 0)	// first time the loop runs
 			 {
 				 min = n;
-			 }
-			 if (n > max)
-			 {
 				 max = n;
+			 } else // if it's not the first time we run the loop check if the min and max have to be updated or not, based on the following input numbers
+			 {
+				 if (n < min)
+			 	{
+					 min = n;
+			 	}
+			 	if (n > max)
+			 	{
+					 max = n;
+			 	}
 			 }
 			 sum += n;
 		 }
